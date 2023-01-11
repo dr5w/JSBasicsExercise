2e004256daaf923a788c0f41ecb8635932674d8c
dr5w <111304827+dr5w@users.noreply.github.com>
2023-01-11 10:42:22 -0600

    completed exercise

---
+++ script.js
@@ -0,0 +1,25 @@
+// JavaScript Basics Exercise
+console.log(7);
+// String, numbers, boolean, null, undefined
+// +, -, *, **, /, %
+console.log((5 % 2) + " " + (5 % 4));
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Hawk" + "eye");
+console.log("Andrew".length + "Garrett".length);
+console.log("Avengers: Endgame"[16]);
+
+// Bonus
+console.log("\\");
+console.log(5 ** 4 - 9);
+console.log("I".length % "Am".length - "Iron".length + "Man".length);
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+m = 25
+c = 4
+u = m + c
